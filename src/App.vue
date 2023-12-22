519056409b206e53d25a2087343e009cb25b18b9
Gabriel Lennon <gabriel.lennon@ifood.com.br>
2023-12-22 16:41:41 -0300

    feat: initial content sctruture

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,101 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import SelectButton from 'primevue/selectbutton';
+  import InputNumber from 'primevue/inputnumber';
+  import Slider from 'primevue/slider';
+  import InputText from 'primevue/inputtext'
+
+  import { ref } from 'vue';
+
+  const activeCoin = ref('EUR');
+  const optionsCoins = ref(['EUR', 'USD']);
+  const inputValueCheck = ref(0);
+  const inputValueTip = ref(10);
+  const inputValuePeople = ref(2);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <h1>Le/Tip</h1>
+    <section class="c-tipp">
+      <div class="c-tip_config">
+        <SelectButton 
+          v-model="activeCoin" 
+          :options="optionsCoins" 
+          aria-labelledby="basic" 
+        />
+        <div class="c-tip_account_value">
+          <label 
+            for="currency-us" 
+            class="font-bold block mb-2"
+          > 
+            Valor
+          </label>
+          <InputNumber 
+            v-model="inputValueCheck" 
+            inputId="currency-us" 
+            mode="currency" 
+            currency="USD" 
+            locale="en-US" 
+          />
+        </div>
+        <div class="c-tip_value">
+          <div>
+            <p>Gorjeta</p>
+            <InputNumber 
+              v-model.number="inputValueTip" 
+              class="w-full" 
+              :min="10"
+              :max="20"
+            />
+            <span>(%)</span>
+          </div>
+          <Slider 
+            v-model="inputValueTip" 
+            class="w-14rem" 
+            :min="10"
+            :max="20"
+          />
+        </div>
+        <div class="c-tip_people">
+          <div>
+            <p>Pessoas</p>
+            <InputNumber 
+              v-model.number="inputValuePeople" 
+              class="w-full" 
+              :min="2"
+              :max="16"
+            />
+          </div>
+          <Slider 
+            v-model="inputValuePeople" 
+            class="w-14rem" 
+            :min="10"
+            :max="20"
+          />
+        </div>
+      </div>
+      <div class="c-tip_info">
+        <div>
+          <p>Conta</p>
+          <p><span>$</span>14</p>
+        </div>
+        <div>
+          <p>Gorjeta</p>
+          <p><span>$</span>14</p>
+        </div>
+        <div>
+          <p>Total</p>
+          <p><span>$</span>14</p>
+        </div>
+        <div>
+          <p>Por pessoa</p>
+          <p><span>$</span>14</p>
+        </div>
+        <div>
+          <p>Em R$</p>
+          <p><span>R$</span>14</p>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import './assets/main.css'
+// import './assets/main.css'
+import "primevue/resources/themes/saga-blue/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core CSS
+import "primeicons/primeicons.css"; //icons
 
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(PrimeVue)
+  .mount("#app");
