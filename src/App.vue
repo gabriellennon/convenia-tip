075f2f50dd41848e0c7fb1bfd68b571e6e8b287d
Gabriel Lennon <gabriel.lennon@ifood.com.br>
2023-12-24 15:44:04 -0300

    refactor: adjust style mobile

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   import SelectButton from 'primevue/selectbutton';
   import InputNumber from 'primevue/inputnumber';
   import Slider from 'primevue/slider';
+  import Button from 'primevue/button';
   import gql from 'graphql-tag';
   import { useQuery } from '@vue/apollo-composable'
   import type { CurrencyConversion } from '@/utils/types'
@@ -19,6 +20,12 @@
   const valuePerPerson = computed(() => {
     return totalCheck.value / inputValuePeople.value;
   });
+
+  const showConfigContent = ref(true);
+
+  const toggleShowConfigContent = () => {
+    showConfigContent.value = !showConfigContent.value;
+  };
 
   const ALL_CURRENCY_CODES = gql`
     query GetCurrencyConversion ($baseCurrency: String! $convertCurrencies: [String]!) {
@@ -53,7 +60,7 @@
   const currency = computed<CurrencyConversion | null>(() => result.value)
 
   watchEffect(() => {
-    console.log(currency)
+    // console.log(currency)
   })
 </script>
 
@@ -68,76 +75,87 @@
         Loading...
       </div>
       <template v-else>
-        <div class="c-tip_config">
-          <SelectButton 
-            v-model="activeCoin" 
-            :options="optionsCoins" 
-            aria-labelledby="basic" 
-          />
-          <div class="c-tip_account_value">
-            <label 
-              for="currency-us" 
-              class="c-tip_label"
-            > 
-              Valor
-            </label>
-            <InputNumber 
-              v-model="inputValueCheck" 
-              inputId="currency-us" 
-              mode="currency" 
-              :currency="activeCoin" 
-              locale="en-US" 
+        <div class="c-tip_c_infos">
+          <div class="c-tip_config" :class="{ visible: showConfigContent }">
+            <SelectButton 
+              v-model="activeCoin" 
+              :options="optionsCoins" 
+              aria-labelledby="basic" 
             />
-          </div>
-          <div class="c-tip_value">
-            <div class="c-tip_value__info">
-              <p class="c-tip_label"> 
-                Gorjeta:
-              </p>
-              <p class="c-tip-value_info_slider">{{ inputValueTip }}</p>
-              <span class="c-tip_label">(%)</span>
+            <div class="c-tip_account_value">
+              <label 
+                for="currency-us" 
+                class="c-tip_label"
+              > 
+                Valor
+              </label>
+              <InputNumber 
+                v-model="inputValueCheck" 
+                inputId="currency-us" 
+                mode="currency" 
+                :currency="activeCoin" 
+                locale="en-US" 
+              />
             </div>
-            <Slider
-              v-model="inputValueTip" 
-              :min="10"
-              :max="20"
-            />
-          </div>
-          <div class="c-tip_people">
-            <div class="c-tip_value__info">
-              <p class="c-tip_label"> 
-                Pessoas:
-              </p>
-              <p class="c-tip-value_info_slider">{{ inputValuePeople }}</p>
+            <div class="c-tip_value">
+              <div class="c-tip_value__info">
+                <p class="c-tip_label"> 
+                  Gorjeta:
+                </p>
+                <p class="c-tip-value_info_slider">{{ inputValueTip }}</p>
+                <span class="c-tip_label">(%)</span>
+              </div>
+              <Slider
+                v-model="inputValueTip" 
+                :min="10"
+                :max="20"
+              />
             </div>
-            <Slider 
-              v-model="inputValuePeople" 
-              :min="2"
-              :max="16"
-            />
+            <div class="c-tip_people">
+              <div class="c-tip_value__info">
+                <p class="c-tip_label"> 
+                  Pessoas:
+                </p>
+                <p class="c-tip-value_info_slider">{{ inputValuePeople }}</p>
+              </div>
+              <Slider 
+                v-model="inputValuePeople" 
+                :min="2"
+                :max="16"
+              />
+            </div>
           </div>
-        </div>
-        <div class="c-tip_info">
-          <div class="c-tip-info_c_values">
-            <p class="c-tip-info_c_values__text">Valor da Conta</p>
-            <p class="c-tip-info_c_values__value__text"><span>$</span>{{ inputValueCheck.toFixed(2) }}</p>
+          <div class="c-tip_info" :class="{ visible: !showConfigContent }">
+            <div class="c-tip-info_c_values">
+              <p class="c-tip-info_c_values__text">Valor da Conta</p>
+              <p class="c-tip-info_c_values__value__text"><span>$</span>{{ inputValueCheck.toFixed(2) }}</p>
+            </div>
+            <div class="c-tip-info_c_values">
+              <p class="c-tip-info_c_values__text">Valor da Gorjeta</p>
+              <p class="c-tip-info_c_values__value__text"><span>$</span>{{ totalTip.toFixed(2) }}</p>
+            </div>
+            <div class="c-tip-info_c_values">
+              <p class="c-tip-info_c_values__text">Total da conta</p>
+              <p class="c-tip-info_c_values__value__text"><span>$</span>{{ totalCheck.toFixed(2) }}</p>
+            </div>
+            <div class="c-tip-info_c_values">
+              <p class="c-tip-info_c_values__text">Por pessoa</p>
+              <p class="c-tip-info_c_values__value__text"><span>$</span>{{ valuePerPerson.toFixed(2) }}</p>
+            </div>
+            <div class="c-tip-info_c_values">
+              <p class="c-tip-info_c_values__text">Em R$</p>
+              <p class="c-tip-info_c_values__value__text"><span>R$</span>14</p>
+            </div>
           </div>
-          <div class="c-tip-info_c_values">
-            <p class="c-tip-info_c_values__text">Valor da Gorjeta</p>
-            <p class="c-tip-info_c_values__value__text"><span>$</span>{{ totalTip.toFixed(2) }}</p>
-          </div>
-          <div class="c-tip-info_c_values">
-            <p class="c-tip-info_c_values__text">Total da conta</p>
-            <p class="c-tip-info_c_values__value__text"><span>$</span>{{ totalCheck.toFixed(2) }}</p>
-          </div>
-          <div class="c-tip-info_c_values">
-            <p class="c-tip-info_c_values__text">Por pessoa</p>
-            <p class="c-tip-info_c_values__value__text"><span>$</span>{{ valuePerPerson.toFixed(2) }}</p>
-          </div>
-          <div class="c-tip-info_c_values">
-            <p class="c-tip-info_c_values__text">Em R$</p>
-            <p class="c-tip-info_c_values__value__text"><span>R$</span>14</p>
-          </div>
+  
+          <Button 
+            class="mobile-button" 
+            :icon="showConfigContent === true ? 'pi pi-arrow-right' : 'pi pi-arrow-left'" 
+            aria-label="Ver mais" 
+            severity="info" 
+            rounded
+            @click="toggleShowConfigContent"
+          ></Button>
         </div>
       </template>
     </section>
@@ -150,56 +168,94 @@
   }
 
   .c-tip {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 1rem;
     padding: 1.5rem;
     border: 1px solid #01b252;
     background: #FBFCFC;
     border-radius: 0.6rem;
 
-    & .c-tip_config{
-      display: flex;
-      flex-direction: column;
+    .c-tip_c_infos {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
       gap: 1rem;
 
-      & .c-tip_label {
-        color: #50555A;
-        font-weight: 500;
-        font-size: 1rem;
-      }
-
-      & .c-tip_value__info {
+      & .c-tip_config{
         display: flex;
-        align-items: center;
-        gap: 0.3rem;
-        margin-bottom: 0.6rem;
+        flex-direction: column;
+        gap: 1rem;
+  
+        & .c-tip_label {
+          color: #50555A;
+          font-weight: 500;
+          font-size: 1rem;
+        }
+  
+        & .c-tip_value__info {
+          display: flex;
+          align-items: center;
+          gap: 0.3rem;
+          margin-bottom: 0.6rem;
+        }
+  
+        & .c-tip-value_info_slider {
+          font-size: 1rem;
+          font-weight: 700;
+          color: #3370aa;
+        }
       }
-
-      & .c-tip-value_info_slider {
-        font-size: 1rem;
-        font-weight: 700;
-        color: #3370aa;
+  
+      & .c-tip_info {
+        text-align: center;
+  
+        .c-tip-info_c_values {
+          & .c-tip-info_c_values__text {
+            font-size: 1rem;
+            font-weight: 500;
+            color: #50555A;
+          }
+  
+          & .c-tip-info_c_values__value__text {
+            font-size: 1.2rem;
+            font-weight: 700;
+            color: #01b252;
+            font-style: italic;
+          }
+        }
+      }
+  
+      & .mobile-button {
+        display: none;
       }
     }
 
-    & .c-tip_info {
-      text-align: center;
+  }
 
-      .c-tip-info_c_values {
-        & .c-tip-info_c_values__text {
-          font-size: 1rem;
-          font-weight: 500;
-          color: #50555A;
+  @media (max-width: 768px) {
+    .c-tip {
+      padding: 1rem;
+      
+      .c-tip_c_infos {
+        display: flex;
+        overflow-x: auto;
+        flex-direction: column;
+        gap: 2rem;
+        
+        & .c-tip_config, .c-tip_info {
+          flex: 0 0 100%;
+          display: none;
         }
-
-        & .c-tip-info_c_values__value__text {
-          font-size: 1.2rem;
-          font-weight: 700;
-          color: #01b252;
-          font-style: italic;
+  
+        .c-tip_config.visible {
+          display: block;
+        }
+        .c-tip_info.visible {
+          display: block;
+        }
+  
+        & .mobile-button {
+          display: block;
         }
       }
+
     }
   }
 </style>
